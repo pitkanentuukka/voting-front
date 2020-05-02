52bc0668290738b604f8753fa16280e74db7096c
tuukka <tuukka@thinkpad>
2020-05-02 11:32:59 +0300

    moved questionlist away from app.js, added react-router-dom dependency

--- src/App.js
+++ src/App.js
@@ -1,55 +1,13 @@
 import React from 'react';
-import Question from './Question';
+import QuestionList from './QuestionList';
 import './App.css';
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      questions: [],
-      answers: []
-      /*answers: [{id: "", value:""}]*/
-    }
-    this.handleChange = this.handleChange.bind(this)
-  }
 
-  handleChange(event) {
-    const {name, value} = event.target
-    this.setState(prevState => {
-      let answers = [...prevState.answers]
-      answers[name] = value
-      console.log(answers)
-      this.setState(answers)
-
-    })
-  }
-
-  componentDidMount() {
-    fetch("/api/questions", {
-      mode: "cors",
-      headers: {
-        'Access-Control-Allow-Origin':'*'
-      }
-    })
-      .then(response => response.json())
-      .then(data =>  {
-
-        this.setState({
-          questions: data
-        })
-
-      })
-  }
-  render () {
-    const questionComponents = this.state.questions.map(question =>
-     <Question key={question.id} id={question.id} question={question.question} handleChange = {this.handleChange} />)
-
-    return (
-      <div>
-        <h1> Questions!</h1>
-        {questionComponents}
-      </div>
-    )
-  }
-
+function App() {
+  return (
+    <QuestionList />
+  )
 }
+
+
+
 export default App;
---
+++ src/QuestionList.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Question from './Question';
+
+class QuestionList extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      questions: [],
+      answers: []
+      /*answers: [{id: "", value:""}]*/
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    const {name, value} = event.target
+    this.setState(prevState => {
+      let answers = [...prevState.answers]
+      answers[name] = value
+      console.log(answers)
+      this.setState(answers)
+
+    })
+  }
+
+  componentDidMount() {
+    fetch("/api/questions", {
+      mode: "cors",
+      headers: {
+        'Access-Control-Allow-Origin':'*'
+      }
+    })
+      .then(response => response.json())
+      .then(data =>  {
+
+        this.setState({
+          questions: data
+        })
+
+      })
+  }
+  render () {
+    const questionComponents = this.state.questions.map(question =>
+     <Question key={question.id} id={question.id} question={question.question} handleChange = {this.handleChange} />)
+
+    return (
+      <div>
+        <h1> Questions!</h1>
+        {questionComponents}
+      </div>
+    )
+  }
+
+}
+export default QuestionList;
