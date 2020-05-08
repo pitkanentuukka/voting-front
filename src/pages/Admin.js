170b87f0e79bef9178cd93a154403dd9a1ca7313
tuukka <tuukka@thinkpad>
2020-05-08 16:41:38 +0300

    added authentication api call and redirect on failure

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,8 +1,54 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 
-function Admin() {
-  return(
-    <h1>Hello Admin</h1>
-  )
+class Admin extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      // when these both are false the user is in kind of a limbo
+      // either one will have to be true for something to happen
+      redirect: false,
+      authorized: false,
+    }
+  }
+
+  componentDidMount(){
+    fetch('/api/authenticate/auth', {
+      mode: "cors",
+      headers: {
+        'Access-Control-Allow-Origin':'*'
+      }
+    })
+    .then(response => {
+      console.log(response.status)
+      if (response.status === 403) {
+        // forbidden, redirect
+        this.setState({redirect : true})
+      } else if(response.status === 200) {
+        // if not redirect, load the admin components or data for them?
+        this.setState({authorized : true})
+      }
+
+    })
+  }
+  render(){
+    if (this.state.redirect) {
+        return(
+           <Redirect to="/" />
+
+        )
+    }
+
+    if (this.state.authorized) {
+      return(
+        <h1>Hello Admin</h1>
+      )
+    }
+    if (!this.state.authorized && !this.state.redirect) {
+      return (
+        <h1>authorizing, please wait</h1>
+      )
+    }
+  }
 }
 export default Admin
