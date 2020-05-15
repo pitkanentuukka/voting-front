50c5e929eec7d9357f83e4c07d38abfa4e138fbd
tuukka <tuukka@thinkpad>
2020-05-15 14:58:02 +0300

    update works now

--- src/pages/AdminQuestion.js
+++ src/pages/AdminQuestion.js
@@ -3,16 +3,51 @@ import React from "react"
 class AdminQuestion extends React.Component {
   constructor(props) {
     super(props)
-
   }
 
+
   render() {
-    return (
-      <div className="question">
+    if (this.props.edit === undefined || this.props.edit === false) {
+      return (
+        <div className="question">
         <p><span>{this.props.question}  </span>
-        <button name={this.props.id} onClick={this.props.handleDelete}>delete</button></p>
-      </div>
-    )
+        <button
+        name={this.props.id}
+        onClick={this.props.handleDelete}>
+        Delete
+        </button>
+        <button
+        name={this.props.id}
+        onClick={this.props.enableEdit}>
+        Edit
+        </button>
+        </p>
+
+        </div>
+      )
+
+    } else {
+      return (
+        <div>
+        <input
+        type='text'
+        name={this.props.id}
+        value={this.props.edit}
+        onChange={this.props.handleEdit}>
+        </input>
+        <button
+        name={this.props.id}
+        onClick={this.props.sendEdited}>
+        Submit
+        </button>
+        <button
+        name={this.props.id}
+        onClick={this.props.disableEdit}>
+        Cancel
+        </button>
+        </div>
+      )
+    }
 
   }
 }
--- src/pages/AdminQuestionList.js
+++ src/pages/AdminQuestionList.js
@@ -12,8 +12,12 @@ class AdminQuestionList extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
     this.handleDelete = this.handleDelete.bind(this)
-
+    this.handleEdit = this.handleEdit.bind(this)
+    this.sendEdited = this.sendEdited.bind(this)
+    this.enableEdit = this.enableEdit.bind(this)
+    this.disableEdit = this.disableEdit.bind(this)
   }
+
 
   componentDidMount() {
     fetch("/api/questions", {
@@ -24,10 +28,101 @@ class AdminQuestionList extends React.Component {
     })
       .then(response => response.json())
       .then(data =>  {
+        data.forEach((item) => {
+          item.edit = false
+        })
         this.setState({
           questions: data
         })
       })
+  }
+
+  handleEdit(event) {
+    const {name, value} = event.target
+    this.setState(prevState => {
+      let questions = prevState.questions.map(question => {
+        if (question.id == name) {
+          question.edit = value
+        }
+      })
+      return questions
+    })
+    console.log(this.state.questions.filter(question => {
+      return question.id == name
+    }));
+  }
+
+  sendEdited(event) {
+    const name = event.target.name
+    const editedQuestion = this.state.questions.filter(question => {
+      return question.id == name
+    })
+
+  //  console.log(editedQuestion[0].edit);
+
+    fetch("/api/admin/editquestion/"+ name, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: "POST",
+      body: JSON.stringify({question: editedQuestion[0].edit})
+    })
+    .then(response => {
+      if (response.status === 200) {
+
+        this.setState(prevState => {
+          let questions = this.state.questions.map(question => {
+            if (question.id == name) {
+              // looks like whatever is here doesn't occur synchronously
+              // so we need to call this twice
+              question.question = question.edit
+            }
+          })
+          return questions
+        })
+        this.setState(prevState => {
+          let questions = this.state.questions.map(question => {
+            if (question.id == name) {
+              delete question.edit
+            }
+          })
+          return questions
+        })
+      } else {
+        // maybe let the user try again
+      }
+    })
+
+  }
+
+  enableEdit(event) {
+    const name = event.target.name
+
+    this.setState(prevState => {
+      let questions = prevState.questions.map(question => {
+        if (question.id == name) {
+          question.edit = question.question
+
+        }
+      })
+      return questions
+    })
+    //this.setState(prevState =>  {return {editableQuestion: editableQuestion}})
+  }
+
+
+  disableEdit(event) {
+    const name = event.target.name
+
+    this.setState(prevState => {
+      let questions = prevState.questions.map(question => {
+        if (question.id == name) {
+          delete question.edit
+        }
+      })
+
+      return questions
+    })
   }
 
   handleDelete(event) {
@@ -46,7 +141,6 @@ class AdminQuestionList extends React.Component {
 
   handleChange(event) {
     const {name, value} = event.target
-
     this.setState({[name]: value})
   }
 
@@ -88,7 +182,13 @@ class AdminQuestionList extends React.Component {
       key={question.id}
       id={question.id}
       question={question.question}
+      edit={question.edit}
       handleDelete={this.handleDelete}
+      handleEdit={this.handleEdit}
+      sendEdited={this.sendEdited}
+      enableEdit={this.enableEdit}
+      disableEdit={this.disableEdit}
+
       />)
 
     return (
